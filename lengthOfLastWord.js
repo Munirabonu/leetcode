b8a19d07ced3b3f191c039c9003bc29279d03c3d
Munirabonu <muniratillaxojayeva@gmail.com>
2023-01-27 12:07:10 +0500

    26_yanvar_3day

---
+++ lengthOfLastWord.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function(s) {
+    return s.trim().slice(s.trim().lastIndexOf(" ")+1).length
+};
+let str='salom dunyo xayr '
+console.log();
+
+// bosh joylarni hisobga olmaslik kk 
+// 'salom xayr ' => 4 bolishi kk 
---
+++ searchInsert.js
@@ -0,0 +1,9 @@
+var searchInsert = function (nums, target) {
+    if (nums.includes(target)) {
+       return nums.indexOf(target)
+    }   
+    else {
+        nums.push(target)
+        return nums.sort(a,b=>a-b).indexOf(target)
+    }
+};
